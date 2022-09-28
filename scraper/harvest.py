a9dd63226a3240f961b780c3f539eee550f8484e
Travis <tleedepriest@gmail.com>
2022-09-27 22:27:32 -0400

    Add docstrings, modify inputs/outputs/function params to integrate with luigi pipeline.

--- scraper/harvest.py
+++ scraper/harvest.py
@@ -1,8 +1,14 @@
 import sys
-import datetime as dt
+import csv
 import glob
+import random
+import requests
+import datetime as dt
+from typing import Union
 from pathlib import Path
 from collections import defaultdict
+import pandas as pd
+
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -11,14 +17,20 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from clean.clean_web_extractions import clean_range_of_posts_on_page
+from clean.clean_web_extractions import (
+    clean_range_of_posts_on_page,
+    clean_date_of_post,
+)
 
-def save_gig_links()-> None:
+
+def save_gig_links(cl_gigs_page_dir: str) -> None:
     """
-    Visits the boston craigslist, navigates to gig section, itterates
-    through pages of gigs and saves the pages for later analysis.
+    Visits Boston CraigsList, navigates to gigs section, itterates
+    through gig pages and saves each gig page in cl_gigs_page_dir.
 
     Args:
+        cl_gigs_page_dir:
+            directory to save each page from gigs section
 
     Returns:
         None
@@ -30,31 +42,37 @@ def save_gig_links()-> None:
     driver = webdriver.Firefox()
     driver.get(link)
 
-    gigs_link = driver.find_element(By.CLASS_NAME, 'ggg')
+    gigs_link = driver.find_element(By.CLASS_NAME, "ggg")
     gigs_link.click()
 
-    today = dt.datetime.today().strftime('%Y_%m_%d')
+    today = dt.datetime.today().strftime("%Y_%m_%d")
     on_last_page = False
     page_count = 0
-    work_dir = Path('working_dir') / f'gig_pages_{today}'
-    work_dir.mkdir(exist_ok=True, parents=True)
+    # cl_gigs_page_dir = Path("working_dir") / f"gig_pages_{today}"
+    # cl_gigs_page_dir.mkdir(exist_ok=True, parents=True)
     while not on_last_page:
+        # TODO: Handle timeout
         try:
             gig_post_title = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, 'li.cl-search-result:nth-child(2) > a:nth-child(3)'))
+                EC.element_to_be_clickable(
+                    (
+                        By.CSS_SELECTOR,
+                        "li.cl-search-result:nth-child(2) > a:nth-child(3)",
+                    )
+                )
             )
         except NoSuchElementException:
             print(f"{gig_post_title} post-titles not found!")
 
         page_content = driver.page_source
-        soup = BeautifulSoup(page_content, 'html.parser')
+        soup = BeautifulSoup(page_content, "html.parser")
         gig_posts = soup.find_all(
-            "li", {"class": "cl-search-result cl-search-view-mode-list"})
+            "li", {"class": "cl-search-result cl-search-view-mode-list"}
+        )
 
-        if gig_posts: # double check page is what we want
-            with (
-                work_dir /\
-                f'gig_post_page_{page_count}.html').open('w') as fh:
+        if gig_posts:  # double check page is what we want
+            with (Path(cl_gigs_page_dir)
+                  / f"gig_post_page_{page_count}.html").open("w") as fh:
 
                 fh.write(page_content)
 
@@ -63,56 +81,120 @@ def save_gig_links()-> None:
 
         try:
             next_button = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((
-                    By.CSS_SELECTOR,
-                    'div.cl-search-paginator:nth-child(2) > button:nth-child(4)'))
+                EC.element_to_be_clickable(
+                    (
+                        By.CSS_SELECTOR,
+                        "div.cl-search-paginator:nth-child(2) > button:nth-child(4)",
+                    )
+                )
             )
         except NoSuchElementException:
             print("No page element!")
 
-        page_number = soup.find('span', {'class':"cl-page-number"})
+        page_number = soup.find("span", {"class": "cl-page-number"})
         if page_number is not None:
             # example 2,161 - 2,171 of 2,171
             page_num_range = page_number.text
-            begin, end, end_range = clean_range_of_posts_on_page(
-                page_num_range)
-        if next_button is not None and end!=end_range:
+            begin, end, end_range = clean_range_of_posts_on_page(page_num_range)
+        if next_button is not None and end != end_range:
             next_button.click()
-            page_count+=1
+            page_count += 1
             continue
         else:
-            on_last_page=True
+            on_last_page = True
 
-def extract_links_from_gig_pages(path_to_gig_page_html):
+
+def extract_links_from_gigs_page(path_to_gig_page_html: str
+                                 header: bool,
+                                 csv_output: str)-> None:
     """
+    Appends to csv file the links from a gigs page. If csv does
+    not exist, will create it.
+
+    Args:
+        path_to_gig_page_html:
+            The path to an html file page from the gigs section
+            containing links to the individual posts
+
+        header:
+            determines whether or not to write the header of the file
+
+        csv_output:
+            The path to the csv file.
+    Returns:
+        None
+
+    Raises:
     """
-    top_dir = Path(path_to_gig_page_html).parent.parent
-    csv_output = top_dir / 'links_to_gig_postings.csv'
-    with open(path_to_gig_page_html, 'r') as fh:
+    with open(path_to_gig_page_html, "r") as fh:
         html_content = fh.read()
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
     gig_posts = soup.find_all(
-        "li", {"class": "cl-search-result cl-search-view-mode-list"})
-    #with csv_output.open('w') as fh:
-    #    fh.write()
+        "li", {"class": "cl-search-result cl-search-view-mode-list"}
+    )
+    with Path(csv_output).open('a+', newline='') as fh:
+        fieldnames = ['title', 'link', 'filename',
+                      'date', 'archive_source_path']
+        csv_writer = csv.DictWriter(fh, fieldnames=fieldnames)
+        if header:
+            csv_writer.writeheader()
+        for gig_post in gig_posts:
+            row = defaultdict()
+            a_el = gig_post.find("a")
+            title = a_el.find('span').text
+            link = a_el["href"]
+            time_el = gig_post.find("time")
+            datetime = time_el["datetime"]
+            row['title'] = title
+            row['link'] = link
+            row['filename'] = link.split('/')[-1]
+            row['date'] = clean_date_of_post(datetime)
+            row['archive_source_path'] = path_to_gig_page_html
+            csv_writer.writerow(row)
 
-    for gig_post in gig_posts:
-        #row = {"title":"", "link"}
-        title = gig_post.find('span')
 
-        a_el = gig_post.find('a')
-        link = a_el['href']
-        time_el = gig_post.find('time')
-        datetime = time_el['datetime']
-        print(title)
-        print(a_el)
-        print(link)
-        print(time_el)
-        print(datetime)
+def extract_gig_post(filename: str,
+                     link: str,
+                     gig_posts_folder: Path)-> None:
+    """
+    This downloads an individual gig post from a link into the
+    gig_posts_folder
+
+    Args:
+        filename:
+            the name of the individual HTML file, derived from link.
+        link:
+            the link to the Craiglist Gig Posting.
+        gig_posts_folder:
+            the folder to save the individual file.
+
+    Returns:
+        None
+
+    Raises:
+        requests.Timeout, all other requests exceptions.
+    """
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            with (gig_posts_folder / filename).open('wb') as fh:
+                fh.write(response.content)
+        else:
+            print(f"Response Status Code: {response.status_code}")
+            return
+
+    except requests.Timeout as e:
+        print(str(e))
+    except:
+        raise # catch-all
+
+def analyze_gig_posts():
+    """
+    """
+    soup = BeautifulSoup(response.content, 'html.parser')
+    section = soup.find('section', {'id':'postingbody'})
+    print(section)
 
 if __name__ == "__main__":
-    today = dt.datetime.today().strftime('%Y_%m_%d')
-    work_dir = Path('working_dir') / f'gig_pages_{today}'
-    for file in work_dir.glob('**/*.html'):
-        extract_links_from_gig_pages(file)
-    #main(sys.argv[1])
+    pass
+    #extract_gig_posts()
